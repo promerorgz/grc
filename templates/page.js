85e24916702cf06c5fa0417702f4e59c613f96be
pablo <pa.romerorgz@gmail.com>
2020-07-13 01:21:46 -0500

    major version bump

---
+++ templates/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PAGE_NAME = () => {
+  return (
+    <div>
+      Hello world! Welcome to PAGE_NAME
+    </div>
+  )
+}
+
+export default PAGE_NAME
---
+++ templates/react.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import StyledCOMPONENT_NAME from './styles.js';
+
+const COMPONENT_NAME = () => {
+  return (
+    <div>
+      {/* start coding here */}
+    </div>
+  )
+}
+
+export default COMPONENT_NAME
